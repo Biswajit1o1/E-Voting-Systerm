1d84c322da2003ed4a159e79ebc271c579e64809
Biswajit1o1 <112772958+Biswajit1o1@users.noreply.github.com>
2022-09-03 22:24:53 +0530

    Add files via upload

---
+++ Project/back/connect.php
@@ -0,0 +1,13 @@
+<?php		
+
+$connect = mysqli_connect("localhost","root","","voting")or die("Connection Failed !!!");
+
+if($connect)
+	{
+		echo "Connected";
+	}
+	else
+	{
+		echo "Not Connected";
+	}
+?>
